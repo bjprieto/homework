e91d2b454938c7e7d15d70a94d754dcc22fcd784
bjprieto <bjprieto@ucdavis.edu>
2023-02-07 10:20:06 -0800

    add_07feb23_notes

---
+++ notes/3_07feb23.py
@@ -0,0 +1,22 @@
+"""
+HW 2 Review
+
+Constructing cross/interaction-matrices
+
+for i in range(i):
+	for j in range(x):
+		...
+		
+x = 0 for full matrix
+x = i for half matrix with major diagonal
+x = i+1 for half matrix without major diagonal
+"""
+"""
+"list" ~ "array" for the sake of this class, since .py arrays are unfavorable
+and uncommonly used
+
+lists ~ tuples, except mutable
+
+take note of whether methods mutate data or not,
+	e.g. L.sort() mutates the list into sorted list
+"""
