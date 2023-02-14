016013dce38bf0723340b1978fdda6955480b8a4
bjprieto <bjprieto@ucdavis.edu>
2023-02-14 15:18:35 -0800

    feb14_notes

---
+++ notes/4_14feb23.py
@@ -0,0 +1,7 @@
+"""
+consider each computation's cost to memory/computing power for optimization,
+incl. math, function calls, assignments, esp. for large data files
+
+
+stdout = print(), i.e. from the program running to the terminal view
+"""
