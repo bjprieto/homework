d382e4d5f2787b3370492ec0e9f07ffc82c74f57
bjprieto <bjprieto@ucdavis.edu>
2023-02-16 10:44:31 -0800

    update_notes

---
+++ notes/4_16feb23.py
@@ -0,0 +1,8 @@
+"""
+Filtering by masking
+* low complexity or entropy filter
+* repetitive regions of the sequence are filtered out
+* good example is the dust program > namesake of hw
+* conserves regions of actual information and masks those that have low
+  information to reduce noise and focus on more valuable regions of the seq
+"""
