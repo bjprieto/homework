b255da1665f21614cda5603d7a63d2c540c7527e
bjprieto <bjprieto@ucdavis.edu>
2023-03-01 19:51:39 -0800

    61_v1

--- unit6/61kmers.py
+++ unit6/61kmers.py
@@ -8,6 +8,42 @@
 # Hint: use argparse
 # Hint: use mcb185.read_fasta()
 
+import argparse # to take and process CLI arguments
+import mcb185 # to read fasta
+
+# Initialize argument parser
+parser = argparse.ArgumentParser(
+	description='Reports the kmer counts for a given nucleotide fasta file.'
+)
+
+# Take required arguments
+parser.add_argument('file', type=str, metavar='<path>', 
+	help='nucleotide fasta file')
+parser.add_argument('klen', type=int, metavar='<int>', help='kmer length')
+
+# Finish argument parser setup
+arg = parser.parse_args()
+
+# Get kmer counts
+kmers = {} # dictionary of kmer_identity:count
+
+for line in mcb185.read_fasta(arg.file):
+
+	# Read only the sequence data, skipping the description
+	for seq in line[1:]: 
+	
+		# Window slide for kmers
+		for pos in range(len(seq)-arg.klen+1): 
+			
+			kmer = seq[pos:pos+arg.klen]
+		
+			# Tally kmers
+			if kmer not in kmers: kmers[kmer] = 1
+			else:				  kmers[kmer] += 1
+
+# Print results
+for kmer in sorted(kmers):
+	print(kmer, kmers[kmer])
 
 """
 python3 60kmers.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz 2
